7f1d4ada04301c0ba2ab5514688dc75d070c83ad
JeelKantaria <57638079+JeelKantaria@users.noreply.github.com>
2021-10-03 17:57:16 +0530

    code added

---
+++ bsort.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio;
+    cin.tie(0);
+    cout.tie(0);
+    int x;
+    while(1)
+    {
+        cin>>x;
+        if (x==42) break;
+        else 
+        cout<<x;
+    }
+    
+}
